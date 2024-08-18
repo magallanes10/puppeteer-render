57bfd0e374d020a6e725801d096526bb0b3e961c
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-08-18 16:41:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+// server.js
+const express = require('express');
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 4000;
+const scrapeLogic = async (url, res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--single-process',
+      '--no-zygote',
+    ],
+    executablePath:
+      process.env.NODE_ENV === 'production'
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+  try {
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // Capture screenshot
+    const screenshotBuffer = await page.screenshot();
+
+    // Send screenshot
+    res.setHeader('Content-Type', 'image/png');
+    res.send(screenshotBuffer);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+};
+
+app.get('/ss', async (req, res) => {
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('URL query parameter is required');
+  }
+
+  await scrapeLogic(url, res);
 });
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
